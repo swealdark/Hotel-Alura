150afc1e4241cebdc779d5151d31c46f4b1067bc
Angel <cell9125@gmail.com>
2023-09-15 18:10:53 -0500

    Button Cancel and Edit Cells

--- src/main/java/com/alura/hotelalura/controllers/CustomRegController.java
+++ src/main/java/com/alura/hotelalura/controllers/CustomRegController.java
@@ -5,10 +5,13 @@ import com.alura.hotelalura.factory.ConnectionFactory;
 import com.alura.hotelalura.model.Register;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 
 import java.util.List;
 
@@ -16,16 +19,37 @@ public class CustomRegController {
     @FXML
     public TableView<Register> regTable;
     @FXML
-    public TableColumn<Register,String> checkin;
+    public TableColumn<Register,String> checkin,checkout,paymentMethod;
+    @FXML
+    public TableColumn<Register,String> id;
+    @FXML
+    public TableColumn<Register,Double> mount;
     @FXML
     public void initialize(){
         RegisterDao registerDao = new RegisterDao(new ConnectionFactory().getConnection());
-        ObservableList<Register> data = FXCollections.observableArrayList(
-                new Register("1","15/12/20","16/12/20",150,"Paypal"),
-                new Register("3","15/11/20","16/11/20",150,"Paypal"));
+        ObservableList<Register> data = FXCollections.observableArrayList(registerDao.getReg());
+        id.setCellValueFactory(new PropertyValueFactory<Register,String>("id"));
         checkin.setCellValueFactory(new PropertyValueFactory<Register,String>("checkin"));
+        checkout.setCellValueFactory(new PropertyValueFactory<Register,String>("checkout"));
+        paymentMethod.setCellValueFactory(new PropertyValueFactory<Register,String>("paymentMethod"));
+        mount.setCellValueFactory(new PropertyValueFactory<Register,Double>("mount"));
         regTable.setItems(data);
-
+        checkin.setCellFactory(TextFieldTableCell.forTableColumn());
+        checkout.setCellFactory(TextFieldTableCell.forTableColumn());
+        paymentMethod.setCellFactory(TextFieldTableCell.forTableColumn());
         
+    }
+    @FXML
+    public void editable(){
+
+
+    }
+    @FXML
+    public void cancelEditable(){
+        initialize();
+    }
+    @FXML
+    public void delReg(){
+        System.out.println("Ga");
     }
 }
